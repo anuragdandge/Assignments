f933d487020bbe4e63015ecdfd40e775291ebfd5
anuragdandge-mcoe <121758594+anuragdandge-mcoe@users.noreply.github.com>
2023-01-10 11:44:51 +0530

    Create Assignment8.cpp

---
+++ Assignment8.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+class Float{
+   private: float num;
+   public : 
+   Float(){
+    num = 10.5;
+   }
+   Float(float i){
+    num = i ;
+   }
+   void operator +(Float);
+   void operator -(Float);
+   void operator *(Float);
+   void operator /(Float);
+};
+void Float::operator+(Float d){
+    int m=num+d.num;
+    cout << "\n Addition of two objects = " << m ;
+}
+void Float::operator-(Float d){
+    int m=num-d.num;
+    cout << "\n Subtraction of two objects = " << m ;
+}
+void Float::operator*(Float d){
+    int m=num*d.num;
+    cout << "\n Multiplication of two objects = " << m ;
+}
+void Float::operator/(Float d){
+    int m=num/d.num;
+    cout << "\n Division of two objects = " << m ;
+}
+int main(){
+    Float d1;
+    Float d2(5.5);
+    d1+d2;
+    d1-d2;
+    d1*d2;
+    d1/d2;
+}
