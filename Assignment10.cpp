b72f9b4d2bbe68ca272c8db6102a82143a8f5981
anuragdandge-mcoe <121758594+anuragdandge-mcoe@users.noreply.github.com>
2023-01-10 11:48:35 +0530

    Create Assignment10.cpp

---
+++ Assignment10.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main(){
+   string s1,s2;
+    cout << "Enter First String : ";
+    cin >> s1;
+    cout << "Enter Second String : ";
+    cin >> s2;
+    cout << "First String is : " << s1 << endl;
+    cout << "Second String is : " << s2 << endl;
+    cout << "Concatenation of two strings is : " << s1+s2 << endl;
+}
